072ff46ca99b9747b0bef9a3a948607fdabf2edb
Akash Vaghela <akashvaghela09@gmail.com>
2022-03-24 19:15:18 +0530

    header updated

--- client/src/Component/Header.jsx
+++ client/src/Component/Header.jsx
@@ -1,27 +1,60 @@
 import React from 'react';
 import styles from '../Styles/Header.module.css'
-import {Link, useLocation} from "react-router-dom";
-
+import { Link, useLocation } from "react-router-dom";
+import { useNavigate } from 'react-router';
+import { MdOutlineDashboard } from 'react-icons/md';
+import { BiWallet } from 'react-icons/bi';
+import { BsQuestionCircle, BsGithub } from 'react-icons/bs';
+import { AiOutlineHome } from 'react-icons/ai';
 
 const Header = () => {
     const location = useLocation();
+    let navigate = useNavigate();
+    let path = location.pathname;
+    const handleRoute = (para) => {
+        navigate(`/${para}`)
+    }
+
 
     return (
         <div className={styles.wrapper}>
-             
-            <div className={styles.section1}>
-                <Link to="/" className={location.pathname === "/" ? styles.activeLink : styles.link}>
-                    <p className={styles.pageName}>MultiSign</p>
-                </Link> 
-            </div> 
-            <div className={styles.section2}>
-                <Link to="/dashboard" className={location.pathname === "/dashboard" ? styles.activeLink : styles.link}>
-                    <p className={styles.pageName}>Dashboard</p>
-                </Link> 
-                <Link to="/contact-me" className={location.pathname === "/contact-me" ? styles.activeLink : styles.link}>
-                    <p className={styles.pageName}>Contact Me</p>
-                </Link> 
-            </div> 
+            <div className={styles.pageLinkContainer}>
+                <label className={path === "/" ? styles.activeLink : styles.pageLink} onClick={() => handleRoute("")} >HomeHomeHomeHomeHome</label>
+                <div className={styles.routeContainer}>
+                    <label className={path === "/dashboard" ? styles.activeLink : styles.pageLink} onClick={() => handleRoute("dashboard")} >Dashboard</label>
+                    <label className={path === "/contact-me" ? styles.activeLink : styles.pageLink} onClick={() => handleRoute("contact-me")} >Contact</label>
+                </div>
+            </div>
+            <label className={styles.pageLink} onClick={() => handleRoute("dashboard")}></label>
+
+
+            <div className={styles.bottomWrapper}>
+                <label className={styles.iconWrapper}>
+                    <AiOutlineHome 
+                        className={path === "/" ? styles.activeLinkIcon : styles.pageIcon} 
+                        onClick={() => handleRoute("")} 
+                    />
+                    <p>Home</p>
+                </label>
+                <label className={styles.iconWrapper}>
+                    <MdOutlineDashboard 
+                        className={path === "/dashboard" ? styles.activeLinkIcon : styles.pageIcon} 
+                        onClick={() => handleRoute("dashboard")} 
+                    />
+                    <p>Dashboard</p>
+                </label>
+                <label className={styles.iconWrapper}>
+                    <BsQuestionCircle 
+                        className={path === "/contact-me" ? styles.activeLinkIcon : styles.pageIcon} 
+                        onClick={() => handleRoute("contact-me")} 
+                    />
+                    <p>Contact Me</p>
+                </label>
+                <label className={styles.iconWrapper}>
+                    <BsGithub className={path === "/github" ? styles.activeLinkIcon : styles.pageIcon} />
+                    <p>GitHub</p>
+                </label>
+            </div>
         </div>
     )
 }
