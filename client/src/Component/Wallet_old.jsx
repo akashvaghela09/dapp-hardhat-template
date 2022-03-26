02d2a67343e8428270e762afd33752286113588f
Akash Vaghela <akashvaghela09@gmail.com>
2022-03-26 17:07:55 +0530

    walle updated

--- client/src/Component/Wallet_old.jsx
+++ client/src/Component/Wallet_old.jsx
@@ -2,6 +2,8 @@ import React, { useState, useEffect } from "react";
 import styles from "../Styles/Wallet.module.css";
 import { useDispatch, useSelector } from "react-redux";
 import { useLocation, useNavigate } from "react-router-dom";
+import Onboard from "bnc-onboard";
+import WEB3 from "web3";
 import { BsWallet2 } from 'react-icons/bs';
 import { BiCopy } from 'react-icons/bi';
 import { IoWalletOutline } from 'react-icons/io5';
@@ -9,12 +11,12 @@ import { GoLinkExternal } from 'react-icons/go';
 import { FaUserCircle } from 'react-icons/fa';
 import { AiFillCaretUp } from 'react-icons/ai';
 import { setIsAuth, setWallet, setWalletModal } from "../Redux/app/actions"
-import { ethers } from "ethers";
-import { abi } from "../helper";
 
-const Wallet = () => {
+const Wallet_old = () => {
     const dispatch = useDispatch();
     const location = useLocation();
+
+    let web3;
 
     const {
         isAuth,
@@ -22,90 +24,99 @@ const Wallet = () => {
         walletModal
     } = useSelector(state => state.app)
 
+    const onboard = Onboard({
+        dappId: process.env.REACT_APP_ONBOARD_KEY,
+        networkId: 4,
+        darkMode: true,
+        subscriptions: {
+            wallet: wallet => {
+                web3 = new WEB3(wallet.provider);
+            }
+        },
+        walletSelect: {
+            wallets: [
+                {
+                    walletName: "metamask",
+                    preferred: true
+                },
+                {
+                    walletName: "fortmatic",
+                    apiKey: process.env.REACT_APP_FORTMATIC_API_KEY,
+                    preferred: true
+                },
+                {
+                    walletName: "coinbase",
+                    preferred: true
+                },
+                {
+                    walletName: "trust"
+                },
+                {
+                    walletName: "walletConnect",
+                    infuraKey: process.env.REACT_APP_INFURA_ID
+                },
+                {
+                    walletName: "portis",
+                    apiKey: process.env.REACT_APP_PORTIS_API_KEY
+                },
+                {
+                    walletName: "authereum"
+                },
+                {
+                    walletName: "ledger"
+                }
+            ]
+        }
+    })
 
     const connectWallet = async () => {
-        dispatch(setWalletModal(false))
+        await dispatch(setWalletModal(false))
+        await onboard.walletSelect()
+        await onboard.walletCheck()
 
-        let walletObj = { "name": "MetaMask" }
-        walletObj.accounts = await metamask.requestAccounts()
-        walletObj.balance = await metamask.getBalance()
-        walletObj.network = await metamask.chainId()
-        walletObj.isConnected = await metamask.isConnected()
-
-        dispatch(setWallet(walletObj));
-        dispatch(setIsAuth(true))
+        let auth = await check()
+        let currentState = getWalletData()
+   
+        dispatch(setWallet(currentState))
+        dispatch(setIsAuth(auth))
     }
 
-    const disconnectWallet = () => {
-        dispatch(setWalletModal(false))
-        dispatch(setWallet({}))
-        dispatch(setIsAuth(false))
-    }
-
-    const handleWalletConnect = () => {
-        if (window.ethereum) {
-            dispatch(setWalletModal(!walletModal))
-        } else {
-            alert("install metamask extension!!")
+    const check = async () => {
+        try {
+            const readyToTransact = await onboard.walletCheck()
+            if (readyToTransact === true) {
+                return true
+            }
+        } catch {
+            return false
         }
     }
 
-    
+    const disconnectWallet = async () => {
+        dispatch(setWalletModal(false))
+        await onboard.walletReset()
+        let auth = await check()
+        dispatch(setIsAuth(auth))
+    }
 
-    // const check = async () => {
-    //     console.log("check called");
-    //     window.ethereum.request({ method: 'eth_requestAccounts' })
-    //     const ethersProvider = new ethers.providers.Web3Provider(window.ethereum, "any");
-    //     let mySigner = ethersProvider.getSigner();
+    const getWalletData = () => {
+        const currentState = onboard.getState()
+        return currentState
+    }
 
-    //     let contractObj = new ethers.Contract(
-    //         process.env.REACT_APP_CONTRACT_ADDRESS,
-    //         abi,
-    //         mySigner
-    //     );
-
-    //     const name = await contractObj.getName()
-    //     console.log(name);
-
-    //     // await contractObj.setName("Akash")
-
-    //     const name2 = await contractObj.getName()
-    //     console.log(name2);
-    // }
-    
-    const metamask = {
-        requestAccounts: async () => {
-            let accountArr = window.ethereum.request({ method: 'eth_requestAccounts' })
-            return accountArr;
-        },
-        getBalance: async (para = 0) => {
-            let acc = await window.ethereum.request({ method: 'eth_requestAccounts' })
-            let _balance = await window.ethereum.request({
-                method: 'eth_getBalance',
-                params: [acc[para], 'latest']
-            })
-
-            return ethers.utils.formatEther(_balance)
-        },
-        chainId: async () => {
-            let chainHex = window.ethereum.request({ method: 'eth_chainId' })
-            return chainHex;
-        },
-        isConnected: async () => {
-            let connectStatus = window.ethereum.isConnected()
-            return connectStatus;
-        }
+    const handleWalletConnect = async () => {
+        dispatch(setWalletModal(!walletModal))
     }
 
     const networkObj = {
-        "0x1": "MainNet",
-        "0x3": "Ropsten",
-        "0x4": "Rinkeby",
-        "0x42": "Kovan"
+        1: "MainNet",
+        42: "Kovan",
+        3: "Ropsten",
+        4: "Rinkeby"
     }
 
     const addressString = () => {
-        let adr = wallet.accounts[0].split("")
+        let adr = wallet.address.split("")
         let start = adr.slice(0, 3).join("")
         let end = adr.slice(-4).join("")
         let string = `${start}...${end}`
@@ -138,7 +149,7 @@ const Wallet = () => {
             <AiFillCaretUp
                 style={{ visibility: walletModal === false && "hidden" }}
                 className={styles.caretIcon}
-                onClick={() => dispatch(setWalletModal(false))}
+            onClick={() => dispatch(setWalletModal(false))} 
             />
             {
                 walletModal === true && isAuth === true ?
@@ -146,14 +157,14 @@ const Wallet = () => {
                         <FaUserCircle className={styles.walletUser} />
                         <div className={styles.walletAddressDiv}>
                             <p className={styles.walletModalText}>ETH: {addressString()}</p>
-                            <BiCopy className={styles.walletModalIcon} onClick={() =>  navigator.clipboard.writeText(wallet.accounts[0])}/>
-                            <GoLinkExternal className={styles.walletModalIcon} onClick={() => window.open(`https://rinkeby.etherscan.io/address/${wallet.accounts[0]}`, '_blank')}/>
+                            <BiCopy className={styles.walletModalIcon} onClick={() =>  navigator.clipboard.writeText(wallet.address)}/>
+                            <GoLinkExternal className={styles.walletModalIcon} onClick={() => window.open(`https://rinkeby.etherscan.io/address/${wallet.address}`, '_blank')}/>
                         </div>
                         <div className={styles.walletDataDiv}>
                             <p className={styles.walletModalText}>Wallet</p>
                             <label className={styles.walletName}>
                                 <IoWalletOutline className={styles.walletModalIcon} style={{ cursor: "auto", margin: 0 }} />
-                                <p className={styles.walletModalText}>{wallet.name}</p>
+                                <p className={styles.walletModalText}>{wallet.wallet.name}</p>
                             </label>
                         </div>
                         <div className={styles.walletDataDiv}>
@@ -176,4 +187,4 @@ const Wallet = () => {
     );
 };
 
-export { Wallet };
+export { Wallet_old };
